dcad2fa76ae073418e833b22a051b0967b7b3f03
Sergio <sergiorobayorr@gmail.com>
2023-11-07 08:50:52 -0500

    testing method to validate if one rect fits into another

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,18 @@ impl Rectangle {
     fn printing(&self) {
         println!("{}", self.name);
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        if rect.area() <= self.area() {
+            true
+        } else {
+            false
+        }
+    }
 }
 
 fn main() {
@@ -23,24 +35,30 @@ fn main() {
         name: String::from("Hola"),
     };
 
-    rect1.printing();
+    let rect2 = Rectangle {
+        width: 30 * 3,
+        height: 50,
+        name: String::from("Hola"),
+    };
+
+    println!("Does rect2 fit in rect1?: {}", rect1.can_hold(&rect2));
 
     // dbg!(&rect1);
 
     // println!("{:#?}", rect1);
 
-    println!(
-        "The area of the rectangle is {} square pixels",
-        rect1.area()
-    );
+    // println!(
+    //     "The area of the rectangle is {} square pixels",
+    //     rect1.area()
+    // );
 
-    let rect2 = Rectangle {
-        width: 30,
-        height: 50,
-        name: String::from("Hola"),
-    };
+    // let rect2 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    //     name: String::from("Hola"),
+    // };
 }
 
-fn area(rect1: &Rectangle) -> u32 {
-    rect1.width * rect1.height
-}
+// fn area(rect1: &Rectangle) -> u32 {
+//     rect1.width * rect1.height
+// }
