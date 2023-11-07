cb1e25e9d99c37435421b20020aa3065c3de16e1
Sergio <sergiorobayorr@gmail.com>
2023-11-07 07:40:44 -0500

    structs

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+    name: String,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn printing(&self) {
+        println!("{}", self.name);
+    }
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+        name: String::from("Hola"),
+    };
+
+    rect1.printing();
+
+    // dbg!(&rect1);
+
+    // println!("{:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+        name: String::from("Hola"),
+    };
+}
+
+fn area(rect1: &Rectangle) -> u32 {
+    rect1.width * rect1.height
+}
