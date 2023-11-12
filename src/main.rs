7ce3f4ac440e5c3dcd24a16f84714a3e3ae7080f
Sergio <sergiorobayorr@gmail.com>
2023-11-11 20:26:16 -0500

    finished traits subsection

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,75 @@
-// use core::panic;
-// use std::collections::HashMap;
-use std::fs;
-use std::io::{self, Read};
-use std::net::IpAddr;
-use std::{fs::File, io::ErrorKind};
+use std::fmt::{format, Debug, Display};
 
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-struct Point<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
-
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
-    }
-}
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}. {}", self.username, self.content)
+    }
+}
+
+pub fn notify<T: Summary + Display>(item: &T, item2: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn some_function<T: Summary + Display>(item: &T) -> String {
+    String::from("")
+}
+
+pub fn some_function_2<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    1
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("idk whasdlnkasdf dkjas dlka jdak  as"),
+        reply: false,
+        retweet: false,
+    }
+}
+
 fn main() {
-    // GENERIC TYPE, TRAITS AND LIFETIMES
-    let wont_work = Point { x: 5, y: 5 };
-    let number_list = vec![34, 50, 25, 100, 65];
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+    println!("1 new tweet: {}", tweet.summarize());
 }
